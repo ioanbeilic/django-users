94056f8b0063b8f6ae53dc774b3647c6bd30e877
ioanbeilic <ioanbeilic@gmail.com>
2021-01-31 14:55:41 +0100

    Update settings.py

--- todaysmenu/todaysmenu/settings.py
+++ todaysmenu/todaysmenu/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fy4c0lv-x+vr&a*f%v(%11zza2mc-9^dx6)@wmlj5ht02lc!w7'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
